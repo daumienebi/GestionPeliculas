fe351956db62b4fb44f22c236ab63335b47f1c44
daumienebi <ddsakpa@gmail.com>
2022-04-13 01:23:54 +0200

    Feat: Adding the image slider and adding more UI's

--- src/es/daumienebi/gestionpeliculas/views/HomeScreen.java
+++ src/es/daumienebi/gestionpeliculas/views/HomeScreen.java
@@ -1,18 +1,55 @@
 package es.daumienebi.gestionpeliculas.views;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.net.URL;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JRootPane;
+
+import es.daumienebi.gestionpeliculas.dao.mysql.DbConnection;
+import resources.RoundedBorder;
+
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+
+import java.awt.FlowLayout;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.CardLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
+import java.awt.Cursor;
+import javax.swing.JComboBox;
+import java.awt.Dimension;
 import javax.swing.JLabel;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.ComponentOrientation;
 
-public class HomeScreen extends JFrame {
+public class MainScreen {
 
-	private JPanel contentPane;
+	private JFrame frmGestionPeliculas;
+	private JPanel mainPanel;
+	int panelHeight,panelWidth;
+	private JButton startBtn;
 
 	/**
 	 * Launch the application.
@@ -21,8 +58,12 @@ public class HomeScreen extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					HomeScreen frame = new HomeScreen();
-					frame.setVisible(true);
+					//DbConnection.conectar();
+					MainScreen window = new MainScreen();					
+					window.frmGestionPeliculas.setLocationRelativeTo(null);
+					
+					window.frmGestionPeliculas.setVisible(true);
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -31,20 +72,150 @@ public class HomeScreen extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the application.
+	 * @throws UnsupportedLookAndFeelException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws ClassNotFoundException 
 	 */
-	public HomeScreen() {
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		URL url = getClass().getResource("/resources/avengerGif.gif");
-		ImageIcon img = new ImageIcon(url);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+	public MainScreen() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException{
+		UIManager u = new UIManager();
+		/*
+		u.setLookAndFeel(UIManager. getCrossPlatformLookAndFeelClassName());
+		*/
+		//this.frmGestionPeliculas.setDefaultLookAndFeelDecorated(true);
+		//frmGestionPeliculas.setUndecorated(true);
 		
-		JLabel lblNewLabel = new JLabel(img);
-		contentPane.add(lblNewLabel, BorderLayout.CENTER);
+		initialize();
+		//frmGestionPeliculas.getRootPane().setWindowDecorationStyle(JRootPane.COLOR_CHOOSER_DIALOG);
+		//frmGestionPeliculas.getRootPane().setBackground(Color.black);
+		/*
+		DbConnection.connect();
+		
+		ResultSet rs;
+		try {
+			PreparedStatement p = DbConnection.getConexion().prepareStatement("select email from cliente");
+			rs = p.executeQuery();
+			while(rs.next()) {
+				System.out.print(rs.getString("email"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmGestionPeliculas = new JFrame();
+		frmGestionPeliculas.setTitle("Movie Management");
+		frmGestionPeliculas.setResizable(false);
+		frmGestionPeliculas.setBounds(100, 100, 900,600);
+		frmGestionPeliculas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmGestionPeliculas.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		ImageIcon fondo = new ImageIcon(getClass().getResource("/resources/background.jpg"));
+		Image img = fondo.getImage();
+		//escalar la imagen
+		Image imgNuevo = img.getScaledInstance(1024,600,java.awt.Image.SCALE_SMOOTH );
+		//volver a asignarle la imagen redimensionada al icono
+		ImageIcon a =new ImageIcon(imgNuevo);
+		mainPanel = new JPanel() {
+			@Override
+		     protected void paintComponent(Graphics g)
+		     {
+		        super.paintComponent(g);
+		        g.drawImage(a.getImage(), 0, 0, null);
+		     }
+		};
+		frmGestionPeliculas.getContentPane().add(mainPanel);
+		mainPanel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel inferiorPanel = new JPanel();
+		inferiorPanel.setBackground(new Color(188, 143, 143));
+		inferiorPanel.setForeground(Color.LIGHT_GRAY);
+		mainPanel.add(inferiorPanel, BorderLayout.SOUTH);
+		inferiorPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		startBtn = new JButton("Start");
+		startBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				HomeScreen hm = new HomeScreen();
+				hm.setLocationRelativeTo(null);			
+				hm.setVisible(true);
+			}
+		});
+		startBtn.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		//RoundedBorder r = new RoundedBorder();
+		startBtn.setBorder(new RoundedBorder(10));
+		startBtn.setOpaque(true);
+		startBtn.setFocusable(false);
+		startBtn.setFocusPainted(false);
+		startBtn.setBackground(SystemColor.textHighlight);
+		startBtn.setForeground(Color.WHITE);
+		inferiorPanel.add(startBtn);
+		startBtn.setFont(new Font("Trebuchet MS", Font.PLAIN, 40));
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmGestionPeliculas.setJMenuBar(menuBar);
+		JMenu mnNewMenu = new JMenu("Home");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Start");
+		mnNewMenu.add(mntmNewMenuItem_4);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Exit");
+		mntmNewMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Close the window
+				int option;
+				option =JOptionPane.showConfirmDialog(frmGestionPeliculas, "Are you sure you want to exit the app ?");
+				if(option == JOptionPane.YES_OPTION) {
+					System.exit(0);
+				}else {}
+					//do nothing
+				
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenu mnNewMenu_1 = new JMenu("Settings");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenu mnNewMenu_4 = new JMenu("Database");
+		mnNewMenu_1.add(mnNewMenu_4);
+		
+		JMenuItem mntmNewMenuItem_5 = new JMenuItem("Configure Connection");
+		mnNewMenu_4.add(mntmNewMenuItem_5);
+		
+		JMenu mnNewMenu_2 = new JMenu("Help");
+		menuBar.add(mnNewMenu_2);
+		
+		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Help Contents");
+		mnNewMenu_2.add(mntmNewMenuItem_3);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Technical Manual");
+		mnNewMenu_2.add(mntmNewMenuItem_2);
+		
+		JPanel panel_1 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
+		flowLayout.setAlignment(FlowLayout.RIGHT);
+		menuBar.add(panel_1);
+		
+		JLabel lblNewLabel = new JLabel("Select Languaje");
+		panel_1.add(lblNewLabel);
+		lblNewLabel.setBackground(new Color(205, 133, 63));
+		
+		JComboBox comboBox = new JComboBox();
+		panel_1.add(comboBox);
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Spanish", "English", "Pidgin"}));
+		comboBox.setMaximumSize(new Dimension(100, 100));
+		
+		JMenu mnNewMenu_3 = new JMenu((String) null);
+		menuBar.add(mnNewMenu_3);
 	}
 
 }
